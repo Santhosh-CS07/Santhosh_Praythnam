b2c89f6eb90cb3d906f2f9a0ba215d54134dc244
santhosh-dev07 <santhosh.m07@gmail.com>
2022-08-19 10:37:34 +0530

    database updated

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.EXT_PUBLIC_FIREBASE_APP_MID
+    apiKey: "AIzaSyAyG4Y1r2O46HUDMNqutZBX_U3SXn-jC7U",
+    authDomain: "infuture-56121.firebaseapp.com",
+    databaseURL: "https://infuture-56121-default-rtdb.firebaseio.com",
+    projectId: "infuture-56121",
+    storageBucket: "infuture-56121.appspot.com",
+    messagingSenderId: "742889219681",
+    appId: "1:742889219681:web:dd46f8eaa12bdaeec7ec20",
+    measurementId: "G-L70K1DRSRW"
 };
 
 // Initialize Firebase
